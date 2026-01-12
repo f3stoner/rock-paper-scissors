36da33aa67c80c17353af26233d62064ef807321
f3stoner <252489762+f3stoner@users.noreply.github.com>
2026-01-12 15:20:40 -0600

    Add 5 round PLayGame logic

--- rps.js
+++ rps.js
@@ -18,11 +18,42 @@ function getHumanChoice(string) {
 
 //console.log(getHumanChoice());//
 
-let humanScore = 0;
-let computerScore = 0;
-
-
-
 function playGame() {
-    
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice();
+        
+        if (humanChoice === "rock" && computerChoice === "paper") {
+            computerScore++;
+            return alert("You lose! Paper beats Rock. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore++;
+            return alert("You win! Rock beats Scissors. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            return alert("You win! Paper beats Rock. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            computerScore++;
+            return alert("You lose! Scissors beats Paper. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            return alert("You win! Scissors beats paper. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            computerScore++;
+            return alert("You lose! Rock beats Scissors. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+        } else {
+            return alert("You Tied. The score is Player - " + humanScore + " Computer - " + computerScore + "");
+    }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
 }
+
+playGame();
